8df026491b4f4751a371d1bcdaee668c28aeda16
Germano Manzoni <germano12344@gmail.com>
2023-06-24 11:42:51 -0300

    acesso da interface

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,9 +23,14 @@ module CadastroAlunosApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
+    
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001' # Insira a origem permitida aqui
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+    
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
